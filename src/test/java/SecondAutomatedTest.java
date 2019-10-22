cddc77305a82653d1888fffa5b8f800547cfa957
Daniel Maciejewski <Daniel.Maciejewski@rossmann.com.pl>
2019-10-22 11:20:25 +0200

    Initial commit

---
+++ src/test/java/SecondAutomatedTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SecondAutomatedTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/bin/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void mySecondTest() {
+        driver.navigate().to("http://www.seleniumhq.org/");
+
+        String pageTitle = driver.getTitle();
+
+        assertTrue(pageTitle.equals("Selenium – Web Browser Automation"));
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+}
